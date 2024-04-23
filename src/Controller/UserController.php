0d9292430fef48a62fcf8578952d400fb7f19151
Mathieu Ledru <mathieu@darkwood.fr>
2024-04-23 20:36:23 +0200

    Add api platform

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use OpenApi\Attributes as OA;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[OA\RequestBody(new Model(type: User::class, groups: ["create"]))]
+    #[Route('/api/user', name: 'app_user', methods: ['POST'])]
+    public function userCreate(): Response
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
@@ -17,9 +18,11 @@ class User
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank(groups: ["default", "create"])]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $name = null;
 
+    #[Assert\NotBlank(groups: ["default", "create"])]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $firstname = null;
 
